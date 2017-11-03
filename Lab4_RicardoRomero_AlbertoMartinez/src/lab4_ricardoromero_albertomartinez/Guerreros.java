65ea570973e1bd0ea242e9246de0f9cb592287c9
RickyRom <30517351+RickyRom@users.noreply.github.com>
2017-11-03 13:35:05 -0600

    clase guerrero

---
+++ Lab4_RicardoRomero_AlbertoMartinez/src/lab4_ricardoromero_albertomartinez/Guerreros.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4_ricardoromero_albertomartinez;
+
+/**
+ *
+ * @author ricky
+ */
+public class Guerreros {
+    private String nombre;
+    private int edad;
+    private String lugar_naci;
+    private int poder_ataque;
+    private int salud;
+    private int costo;
+
+    public Guerreros() {
+    }
+
+    public Guerreros(String nombre, int edad, String lugar_naci, int poder_ataque, int salud, int costo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.lugar_naci = lugar_naci;
+        this.poder_ataque = poder_ataque;
+        this.salud = salud;
+        this.costo = costo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getLugar_naci() {
+        return lugar_naci;
+    }
+
+    public void setLugar_naci(String lugar_naci) {
+        this.lugar_naci = lugar_naci;
+    }
+
+    public int getPoder_ataque() {
+        return poder_ataque;
+    }
+
+    public void setPoder_ataque(int poder_ataque) {
+        this.poder_ataque = poder_ataque;
+    }
+
+    public int getSalud() {
+        return salud;
+    }
+
+    public void setSalud(int salud) {
+        this.salud = salud;
+    }
+
+    public int getCosto() {
+        return costo;
+    }
+
+    public void setCosto(int costo) {
+        this.costo = costo;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
